86af22b0f815ada63f564eb425d6c1d637e15748
aryan-marthak <aryan.marthak1@gmail.com>
2025-03-16 23:33:57 +0530

    BACKENd JWT TOKEN Complete

--- Backend/route/user.route.js
+++ Backend/route/user.route.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import { signup } from "../controller/user.controller.js";
+import { signup, login, logout } from "../controller/user.controller.js";
 const router = express.Router();
 
-router.post("/signup", signup)
+router.post("/signup", signup);
 
-export default router
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+export default router    
