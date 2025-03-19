56fad0f189e315f786fe887966941ad99fbc30ce
aryan-marthak <aryan.marthak1@gmail.com>
2025-03-19 23:03:46 +0530

    react hook form

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Left from './home/left/Left'
 import Right from './home/right/Right'
 import Logout from './home/left1/Logout'
 import Signup from './components/Signup'
+import Login from './components/Login'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       </div> */}
 
       <Signup/>
+      {/* <Login/> */}
     </>
   )
 }
